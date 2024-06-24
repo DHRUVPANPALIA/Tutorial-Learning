fadbe2d89bc5db16cec9af2bdc5ba8f3111772e5
DHRUVPANPALIA <21dit043@charusat.edu.in>
2024-06-24 13:34:13 +0530

    New Course

---
+++ Backend-Projects/ZeroToMastery-Course__NodeJS/Demo.js
@@ -0,0 +1,9 @@
+// Learnt about the process object and this is exammple of process.argv[]
+
+const mission = process.argv[2];    // Argument is at second index of array
+
+if (mission === 'learn') {
+    console.log('Time to write');
+} else {
+    
+}
